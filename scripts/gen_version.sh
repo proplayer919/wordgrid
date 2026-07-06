ae61ef2f24a5ab1ec8d1619520f672022f39ad42
proplayer919 <pro_player919@outlook.com>
2026-07-06 11:00:29 +1000

    fix script bug

--- scripts/gen_version.sh
+++ scripts/gen_version.sh
@@ -7,7 +7,7 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 COMMIT_NUMBER_THIS_MONTH=$(git rev-list --count --since="$(date +%Y-%m-01)" HEAD)
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-cat <<EOL > ../src/version.ts
+cat <<EOL > src/version.ts
 export const COMMIT: string = '$COMMIT_HASH';
 export const COMMIT_NUMBER_THIS_MONTH: string = '$COMMIT_NUMBER_THIS_MONTH';
 export const BUILD_TIMESTAMP: string = '$TIMESTAMP';
