86d10e6be3580cdb9059b6dc72fd9ce2753774de
proplayer919 <pro_player919@outlook.com>
2026-07-17 19:14:01 +1000

    fix

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,5 +1,5 @@
-export const API_PORT = Number(process.env.API_PORT) || 7000;
-export const MATCHMAKING_PORT = Number(process.env.MATCHMAKING_PORT) || 7001;
+export const API_PORT = Number(process.env.API_PORT) || 8210;
+export const MATCHMAKING_PORT = Number(process.env.MATCHMAKING_PORT) || 8211;
 
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://wordgrid-mongo:27017/wordgrid';
 export const REDIS_HOST = process.env.REDIS_HOST || 'wordgrid-redis';
